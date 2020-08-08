04904284a17b33cedb619e803be168556b5c583d
Sam Amis <30879203+SameAsMuli@users.noreply.github.com>
2020-08-08 23:06:20 +0100

    fix: use correct prefixes

--- src/action/add.cpp
+++ src/action/add.cpp
@@ -3,7 +3,7 @@
 namespace action {
 
 Add::Add(const util::InputParser& input, const std::filesystem::path& file):
-    AddAbstract( "add", "Add a normal priority TODO", input, file, "+")
+    AddAbstract( "add", "Add a normal priority TODO", input, file, "-")
 { }
 
 } // namespace action
--- src/action/low.cpp
+++ src/action/low.cpp
@@ -3,7 +3,7 @@
 namespace action {
 
 Low::Low(const util::InputParser& input, const std::filesystem::path& file):
-    AddAbstract( "low", "Add a low priority TODO", input, file, "-")
+    AddAbstract( "low", "Add a low priority TODO", input, file, "~")
 { }
 
 } // namespace action
