067f320742a48f7ed892eefe8e861c25b55f5e83
Sam Amis <s_amis@hotmail.co.uk>
2021-03-01 00:03:25 +0000

    fix: handle more cases when string wrapping

--- src/util/string.cpp
+++ src/util/string.cpp
@@ -1,4 +1,5 @@
 #include "SafeInt.hpp"
+#include <iostream>
 
 #include "util/string.hpp"
 
@@ -17,12 +18,52 @@ std::string wrap(const std::string &input, int width) {
         throw std::runtime_error{"Invalid width passed to util::string::wrap"};
     }
 
-    std::string::size_type n = 0;
+    std::string::size_type newlinePos = 0;
+    std::string::size_type spacePos = 0;
     auto str = std::string(input);
 
-    while (n != std::string::npos && width < input.length() - n) {
-        n = str.rfind(' ', n + width);
-        str.at(n) = '\n';
+    /* Loop till the last line is less than 'width' number of characters long */
+    while (width < input.length() - spacePos) {
+        /* Check where the last newline character was */
+        newlinePos = str.rfind('\n', spacePos + width);
+
+        if (newlinePos == std::string::npos || newlinePos == spacePos) {
+            /* The current line is longer than 'width' number of characters so
+             * split it at the last space character before exceeding 'width'.
+             */
+            spacePos = str.rfind(' ', spacePos + width);
+
+            if (newlinePos == std::string::npos ||
+                (spacePos != std::string::npos && spacePos > newlinePos)) {
+                str.at(spacePos) = '\n';
+
+            } else {
+                /* There are no space characters in the next 'width' number of
+                 * characters so look for the next appropriate place to split.
+                 */
+                spacePos = str.find(' ', spacePos + width);
+
+                if (spacePos == std::string::npos) {
+                    /* No more spaces in the string, so stop here */
+                    break;
+                }
+
+                newlinePos = str.find('\n', spacePos + width);
+
+                if (spacePos < newlinePos) {
+                    str.at(spacePos) = '\n';
+                } else {
+                    spacePos = newlinePos;
+                }
+            }
+
+        } else {
+            /* There was a newline character within 'width' number of characters
+             * from the start of the line, so we don't need to wrap this line
+             * and can now start counting from that point.
+             */
+            spacePos = newlinePos;
+        }
     }
 
     return str;
