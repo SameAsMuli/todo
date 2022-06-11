10978d912b86ec4c9d2e73fa513ed2cc0e2c39be
Sam Amis <s_amis@hotmail.co.uk>
2022-06-11 14:32:22 -0500

    refactor: simplify ansi colour usage

--- src/util/ansi.hpp
+++ src/util/ansi.hpp
@@ -1,74 +1,20 @@
 #ifndef UTIL_ANSI_H
 #define UTIL_ANSI_H
 
-#include <iostream> // std::basic_ostream
-
-namespace util {
-namespace ansi {
+#include <string> // std::string
 
 // Formatting
-template <class CharT, class Traits>
-constexpr std::basic_ostream<CharT, Traits> &
-reset(std::basic_ostream<CharT, Traits> &os) {
-    return os << "\033[0m";
-}
-
-template <class CharT, class Traits>
-constexpr std::basic_ostream<CharT, Traits> &
-bold(std::basic_ostream<CharT, Traits> &os) {
-    return os << "\033[1m";
-}
+#define ANSI_RESET "\033[0m"
+#define ANSI_BOLD "\033[1m"
 
 // Foreground colours
-template <class CharT, class Traits>
-constexpr std::basic_ostream<CharT, Traits> &
-foreground_black(std::basic_ostream<CharT, Traits> &os) {
-    return os << "\033[30m";
-}
-
-template <class CharT, class Traits>
-constexpr std::basic_ostream<CharT, Traits> &
-foreground_red(std::basic_ostream<CharT, Traits> &os) {
-    return os << "\033[31m";
-}
-
-template <class CharT, class Traits>
-constexpr std::basic_ostream<CharT, Traits> &
-foreground_green(std::basic_ostream<CharT, Traits> &os) {
-    return os << "\033[32m";
-}
-
-template <class CharT, class Traits>
-constexpr std::basic_ostream<CharT, Traits> &
-foreground_yellow(std::basic_ostream<CharT, Traits> &os) {
-    return os << "\033[33m";
-}
-
-template <class CharT, class Traits>
-constexpr std::basic_ostream<CharT, Traits> &
-foreground_blue(std::basic_ostream<CharT, Traits> &os) {
-    return os << "\033[34m";
-}
-
-template <class CharT, class Traits>
-constexpr std::basic_ostream<CharT, Traits> &
-foreground_magenta(std::basic_ostream<CharT, Traits> &os) {
-    return os << "\033[35m";
-}
-
-template <class CharT, class Traits>
-constexpr std::basic_ostream<CharT, Traits> &
-foreground_cyan(std::basic_ostream<CharT, Traits> &os) {
-    return os << "\033[36m";
-}
-
-template <class CharT, class Traits>
-constexpr std::basic_ostream<CharT, Traits> &
-foreground_white(std::basic_ostream<CharT, Traits> &os) {
-    return os << "\033[37m";
-}
-
-} // namespace ansi
-} // namespace util
+#define ANSI_FOREGROUND_BLACK "\033[30m"
+#define ANSI_FOREGROUND_RED "\033[31m"
+#define ANSI_FOREGROUND_GREEN "\033[32m"
+#define ANSI_FOREGROUND_YELLOW "\033[33m"
+#define ANSI_FOREGROUND_BLUE "\033[34m"
+#define ANSI_FOREGROUND_MAGENTA "\033[35m"
+#define ANSI_FOREGROUND_CYAN "\033[36m"
+#define ANSI_FOREGROUND_WHITE "\033[37m"
 
 #endif
